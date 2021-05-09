867b1923cfce58cde7f33c0747fcfa0ad7319410
egypt-doctor <73825698+egypt-doctor@users.noreply.github.com>
2021-05-09 14:12:12 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Neon Entry</title>
+
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+	<body>
+		<div class="enter">
+			<form>
+			<h1>Are You Admin ?</h1>
+			<input type="text" placeholder="Enter A R00m Number">
+			<input type="text" placeholder="KEY ">
+			<button class="btn">Explore </button>
+
+
+
+			</form>
+		</div>
+
+
+	</body>
+</html>
+
